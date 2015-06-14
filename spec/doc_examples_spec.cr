0b585f90f66762afb2d6361d8c1c0ddc813eb0eb
Tobias Pfeiffer <pragtob@gmail.com>
2015-06-14 10:56:45 +0200

    updated specs to see that the ones in teh docs run

--- spec/doc_examples_spec.cr
+++ spec/doc_examples_spec.cr
@@ -1,15 +1,19 @@
 require "spec"
 
-describe "sample specs" do
-  describe "addition" do
-    it "correctly adds one and one" do
-      (1 + 1).should eq 2
+describe "Array" do
+  describe "#length" do
+    it "correctly reports the number of elements in the Array" do
+      [1, 2, 3].length.should eq 3
     end
   end
 
-  describe "empty" do
-    it "is empty" do
+  describe "#empty?" do
+    it "is empty when no elements are in the array" do
       ([] of Int32).empty?.should be_true
+    end
+
+    it "is not empty if there are elements in the array" do
+      [1].empty?.should be_false
     end
   end
 end
